0c1838dd7fdb12af9b5e5a7fbe43bdbaa2581ac7
Lucas Bebber <lucasbbebber@gmail.com>
2019-06-06 16:33:16 -0300

    Change from arrow functions to regular functions, for IE11 compatibility

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 
-export const safelyGetBlockClass = (blockClass: string) =>
-blockClass ? blockClass.split(' ')[0] : ''
+/** TODO: Arrow functions are not being converted to regular functions by Pika,
+ * and thus do not work on IE11. Check if any update on pika/plugin-build-web
+ * changes this behaviour, or change the packager alltogether.
+ * (Keep an eye on https://github.com/pikapkg/builders/pull/52) */
 
-export const generateBlockClass = (baseClass: string, blockClass?: string) =>
-blockClass
-  ? `${baseClass} ${baseClass}--${safelyGetBlockClass(blockClass)}`
-  : baseClass
+export function safelyGetBlockClass(blockClass: string) {
+  blockClass ? blockClass.split(' ')[0] : ''
+}
+
+export function generateBlockClass(baseClass: string, blockClass?: string) {
+  blockClass
+    ? `${baseClass} ${baseClass}--${safelyGetBlockClass(blockClass)}`
+    : baseClass
+}
 
 export interface BlockClass {
   blockClass?: string
